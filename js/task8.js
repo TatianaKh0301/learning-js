aca609ab02ad5b8ff7d0bacfb5ef2ea935116647
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-07-06 15:30:53 +0300

    Update task8.js

--- js/task8.js
+++ js/task8.js
@@ -2,13 +2,19 @@
 // целое число (параметр number) и возвращает сумму всех целых чисел 
 // от единицы и до этого числа. Например, если number равно 3, 
 // то сумма это 1 + 2 + 3, то есть 6.
-let number = 3;
+let number = 5;
+let numberNew =0;
+
 function calculateTotal(number) {
     // Change code below this line
-   for (let i = 1; i <= number; i += 1) {
-        console.log("i = ", i);
-        console.log("number=", number);
-   }   
-//    console.log("summNumber = ", number);
-     // Change code above this line
+
+   for (i = 1; i <= number; i += 1) {
+     numberNew = numberNew + i;
+   } 
+    
+     return;
    }
+   
+   calculateTotal(number);
+
+   console.log("numberNew", numberNew);
