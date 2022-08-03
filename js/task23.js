2a4ae2865fc498eb957769a11b8d299efc2affba
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-03 17:16:44 +0300

    Update task23.js

--- js/task23.js
+++ js/task23.js
@@ -9,13 +9,33 @@ const pizzaPalace = {
         // for (const pizza of this.pizzas) {
         //     console.log("pizzaName", pizzaName);
         //     console.log("pizza", pizza);
-            if(!this.pizzas.includes(pizzaName)) {
-              onError(pizzaName);
-            } else {
-              onSuccess(pizzaName);              
+            // if(!this.pizzas.includes(pizzaName)) {
+            //   onError(pizzaName);
+            // } else {
+            //   onSuccess(pizzaName);              
+            // }
+
+            // this.pizzas.forEach(function (pizza) {
+            //   if(pizza === pizzaName) {
+            //     onSuccess(pizzaName);
+            //   }                              
+            // });
+            // onError(pizzaName);  
+              // console.log("pizza-", pizza);
+              // for (let i = 0; i < this.pizzas.length; i += 1) {
+
+              // }
+              for (const pizza of this.pizzas) {
+                if (pizza === pizzaName) {
+                  onSuccess(pizzaName);
+                  return pizzaName;
+                } 
             }
+            onError(pizzaName);
+            return(pizzaName);
             
-        },        
+            
+    },        
     
   };
   // Change code above this line
@@ -33,5 +53,5 @@ const pizzaPalace = {
   // Method calls with callbacks
 // pizzaPalace.order('Smoked', makePizza, onOrderError);
 // pizzaPalace.order('Four meats', makePizza, onOrderError);
-pizzaPalace.order('Big Mike', makePizza, onOrderError);
-// pizzaPalace.order('Vienna', makePizza, onOrderError);
+// pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
